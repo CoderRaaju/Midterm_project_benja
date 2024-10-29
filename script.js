dc848e7716f697441c56c9c415f7925c33b77a84
CoderRaaju <rbal7479@gmail.com>
2024-10-30 01:03:41 +0900

    lol

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you shortly.');
+});
